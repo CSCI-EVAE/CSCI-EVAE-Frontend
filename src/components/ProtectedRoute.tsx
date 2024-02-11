5af77972d3a1b25c78791ec39ce656c305740e00
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-02-11 14:55:45 +0100

    init the the project themplate

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { hasRole } from '../utils/authUtils';
-import {AuthResponse, User} from "../types/authTypes";
 
 interface ProtectedRouteProps {
     children: React.ReactNode;
